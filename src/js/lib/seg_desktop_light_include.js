0490b375537dc56b692e74846a7046c189ba3572
Threepwood <scope.dk@gmail.com>
2018-10-04 14:13:08 +0200

    Update javascript

---
+++ src/js/lib/seg_desktop_light_include.js
@@ -0,0 +1,28 @@
+document.write('<script type="text/javascript" src="/js/manipulator/src/u.js"></script>');
+document.write('<script type="text/javascript" src="/js/manipulator/src/u-debug.js"></script>');
+
+document.write('<script type="text/javascript" src="/js/manipulator/src/u-cookie.js"></script>');
+document.write('<script type="text/javascript" src="/js/manipulator/src/u-dom.js"></script>');
+document.write('<script type="text/javascript" src="/js/manipulator/src/u-events.js"></script>');
+document.write('<script type="text/javascript" src="/js/manipulator/src/u-events-browser.js"></script>');
+document.write('<script type="text/javascript" src="/js/manipulator/src/u-geometry.js"></script>');
+document.write('<script type="text/javascript" src="/js/manipulator/src/u-init.js"></script>');
+document.write('<script type="text/javascript" src="/js/manipulator/src/u-math.js"></script>');
+document.write('<script type="text/javascript" src="/js/manipulator/src/u-preloader.js"></script>');
+document.write('<script type="text/javascript" src="/js/manipulator/src/u-request.js"></script>');
+document.write('<script type="text/javascript" src="/js/manipulator/src/u-string.js"></script>');
+document.write('<script type="text/javascript" src="/js/manipulator/src/u-system.js"></script>');
+document.write('<script type="text/javascript" src="/js/manipulator/src/u-timer.js"></script>');
+document.write('<script type="text/javascript" src="/js/manipulator/src/u-url.js"></script>');
+
+document.write('<script type="text/javascript" src="/js/manipulator/src/u-array-desktop_light.js"></script>');
+document.write('<script type="text/javascript" src="/js/manipulator/src/u-dom-desktop_ie10.js"></script>');
+document.write('<script type="text/javascript" src="/js/manipulator/src/u-dom-desktop_light.js"></script>');
+document.write('<script type="text/javascript" src="/js/manipulator/src/u-events-desktop_light.js"></script>');
+document.write('<script type="text/javascript" src="/js/manipulator/src/u-geometry-desktop_light.js"></script>');
+document.write('<script type="text/javascript" src="/js/manipulator/src/u-json-desktop_light.js"></script>');
+document.write('<script type="text/javascript" src="/js/manipulator/src/u-preloader-desktop_light.js"></script>');
+document.write('<script type="text/javascript" src="/js/manipulator/src/u-request-desktop_light.js"></script>');
+document.write('<script type="text/javascript" src="/js/manipulator/src/u-string-desktop_light.js"></script>');
+
+
