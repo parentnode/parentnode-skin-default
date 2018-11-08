e85465bc856fd20bb7624090f228cc43a937601f
Martin Kæstel Nielsen <martin@think.dk>
2018-11-08 15:29:08 +0100

    Restructure skin

--- js/seg_seo.js
+++ js/seg_seo.js
@@ -1,4 +1,4 @@
 /*
 Manipulator v0.9.2-full Copyright 2017 http://manipulator.parentnode.dk
-js-merged @ 2018-11-07 00:55:21
+asset-builder @ 2018-11-08 15:28:35
 */
--- js/seg_tablet.js
+++ js/seg_tablet.js
@@ -1,6 +1,6 @@
 /*
 Manipulator v0.9.2-full Copyright 2017 http://manipulator.parentnode.dk
-js-merged @ 2018-11-07 00:55:21
+asset-builder @ 2018-11-08 15:28:34
 */
 
 /*seg_tablet_include.js*/
