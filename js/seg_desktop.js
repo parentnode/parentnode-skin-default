ab32627bf0a393730923c2e28859094aacd941a2
Søren Lyhne <soren@parentnode.dk>
2018-11-14 10:25:56 +0100

    add torvehallerne.png + build

--- js/seg_desktop.js
+++ js/seg_desktop.js
@@ -1,6 +1,6 @@
 /*
 Manipulator v0.9.2-full Copyright 2017 http://manipulator.parentnode.dk
-asset-builder @ 2018-11-12 15:57:18
+asset-builder @ 2018-11-14 10:24:19
 */
 
 /*seg_desktop_include.js*/
