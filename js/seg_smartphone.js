26c11afe7c29e5f401a9707652c2cf5498e8efc3
Søren Lyhne <soren@parentnode.dk>
2018-11-30 15:55:18 +0100

    build

--- js/seg_smartphone.js
+++ js/seg_smartphone.js
@@ -1,6 +1,6 @@
 /*
 Manipulator v0.9.2-full Copyright 2017 http://manipulator.parentnode.dk
-asset-builder @ 2018-11-30 14:44:32
+asset-builder @ 2018-11-30 15:54:28
 */
 
 /*seg_smartphone_include.js*/
