4710148e65391f0fd8998e396028ed0ed4f1ad59
threepwoodd <scope.dk@gmail.com>
2018-12-04 14:49:01 +0100

    Build

--- js/seg_seo.js
+++ js/seg_seo.js
@@ -1,4 +1,4 @@
 /*
 Manipulator v0.9.2-full Copyright 2017 http://manipulator.parentnode.dk
-asset-builder @ 2018-12-04 14:24:37
+asset-builder @ 2018-12-04 14:48:53
 */
--- js/seg_tablet.js
+++ js/seg_tablet.js
@@ -1,6 +1,6 @@
 /*
 Manipulator v0.9.2-full Copyright 2017 http://manipulator.parentnode.dk
-asset-builder @ 2018-12-04 14:24:37
+asset-builder @ 2018-12-04 14:48:53
 */
 
 /*seg_tablet_include.js*/
