2a73beac11599ad0f36a9b92ebf8d31c8956cbb0
threepwoodd <scope.dk@gmail.com>
2019-03-29 14:21:27 +0100

    Build

--- js/seg_tablet.js
+++ js/seg_tablet.js
@@ -1,6 +1,6 @@
 /*
 Manipulator v0.9.2-full Copyright 2017 http://manipulator.parentnode.dk
-asset-builder @ 2019-03-29 14:10:04
+asset-builder @ 2019-03-29 14:21:22
 */
 
 /*seg_tablet_include.js*/
