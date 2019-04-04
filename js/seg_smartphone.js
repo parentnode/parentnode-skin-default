9521b984698ba5b435909433da656c8094ee74cd
threepwoodd <scope.dk@gmail.com>
2019-04-04 02:27:21 +0200

    Build

--- js/seg_smartphone.js
+++ js/seg_smartphone.js
@@ -1,6 +1,6 @@
 /*
 Manipulator v0.9.2-full Copyright 2017 http://manipulator.parentnode.dk
-asset-builder @ 2019-04-04 02:26:22
+asset-builder @ 2019-04-04 02:27:15
 */
 
 /*seg_smartphone_include.js*/
--- js/seg_tablet.js
+++ js/seg_tablet.js
@@ -1,6 +1,6 @@
 /*
 Manipulator v0.9.2-full Copyright 2017 http://manipulator.parentnode.dk
-asset-builder @ 2019-04-04 02:26:22
+asset-builder @ 2019-04-04 02:27:16
 */
 
 /*seg_tablet_include.js*/
