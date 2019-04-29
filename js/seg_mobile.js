9a2b3197e8db792e8fdca813f592f6e1d52084d8
threepwoodd <scope.dk@gmail.com>
2019-04-29 17:32:02 +0200

    Build

--- js/seg_mobile.js
+++ js/seg_mobile.js
@@ -1,6 +1,6 @@
 /*
 MIT license, 2019 parentNode.dk
-asset-builder @ 2019-04-26 12:57:03
+asset-builder @ 2019-04-29 17:31:55
 */
 
 /*seg_mobile_include.js*/
--- js/seg_seo.js
+++ js/seg_seo.js
@@ -1,4 +1,4 @@
 /*
 MIT license, 2019 parentNode.dk
-asset-builder @ 2019-04-26 12:57:03
+asset-builder @ 2019-04-29 17:31:55
 */
